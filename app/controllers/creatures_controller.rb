23a42c27fce6e0bc43b72ec3655e5cf5a1f74541
jchurley95 <jchurley95@gmail.com>
2017-09-06 12:33:56 -0400

    First commit

---
+++ app/controllers/creatures_controller.rb
@@ -0,0 +1,50 @@
+class CreaturesController < ApplicationController
+  def show
+    @creature = Creature.find params[:id]
+  end
+
+  def index
+    @creatures = Creature.all
+  end
+
+  def edit
+    @creature = Creature.find params[:id]
+  end
+
+  def new
+    @creature = Creature.new
+
+  end
+
+  def create
+    @creature = Creature.new(creature_params)
+    
+    if @creature.save
+      redirect_to creature_path(@creature)
+    end
+  end
+
+  def update
+    @creature = Creature.find params [:id]
+
+    if @creature.update(creature_params)
+      redirect_to creature_path(@creature)
+    else 
+      render :edit
+    end
+    
+  end
+
+  def destroy
+    @creature = Creature.find params [:id]
+    @creature.destroy
+    redirect_to creatures_path
+    
+  end
+
+  private
+  def creature_params
+    creature_params = params.require(:creature).permit(:name, :description)
+  end
+
+end
